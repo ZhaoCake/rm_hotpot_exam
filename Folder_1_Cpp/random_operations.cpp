dcb325a1e21e256e9d0020c7df1c4167eb99ca32
zhaocake <zy9787101052763@126.com>
2023-08-10 14:11:37 +0800

    Suit the rule of pre-commit

--- Folder_1_Cpp/random_operations.cpp
+++ Folder_1_Cpp/random_operations.cpp
@@ -1,64 +1,62 @@
+// Copyright 2023 <ZhaoCake>
+#include <unistd.h>
 #include <vector>
 #include <iostream>
 #include <functional>
 
 
 // 创建随即数据，可指定数据个数
-std::vector<float> createRandomData(int num)
-{
+std::vector<float> createRandomData(int num) {
+    u_int seed = time(NULL);
     std::vector<float> data;
-    for (int i = 0; i < num; i++)
-    {
-        data.push_back(rand() % num);
+    for (int i = 0; i < num; i++) {
+        data.push_back(rand_r(&seed) % num);
     }
     return data;
 }
 
 // 操作1：求和
-float operation1(std::vector<float>& data)
-{
+float operation1(const std::vector<float>& data) {
     float sum = 0;
-    for (int i = 0; i < data.size(); i++)
-    {
+    for (int i = 0; i < data.size(); i++) {
         sum += data[i];
     }
     return sum;
 }
 
 // 操作2：求均值
-float operation2(std::vector<float>& data)
-{
+float operation2(const std::vector<float>& data) {
     auto sum = operation1(data);
     auto mean = sum / data.size();
     return mean;
 }
 
 // 操作3：求方差
-float operation3(std::vector<float>& data)
-{
+float operation3(const std::vector<float>& data) {
     auto mean = operation2(data);
     float sum = 0;
-    for (int i = 0; i < data.size(); i++)
-    {
+    for (int i = 0; i < data.size(); i++) {
         sum += (data[i] - mean) * (data[i] - mean);
     }
     return sum;
 }
 
 // 操作4：求累乘
-float operation4(std::vector<float>& data)
-{
+float operation4(const std::vector<float>& data) {
     auto product = 1;
-    for (int i = 0; i < data.size(); i++)
-    {
+    for (int i = 0; i < data.size(); i++) {
         product *= data[i];
     }
     return product;
 }
 
 // 定义定时器，定时器每500ms执行一次，每次执行随机选择一种操作
-float createTimerLoop(int period, std::vector<float>& data, std::vector<std::function<float (std::vector<float>&)>>& operations)
-{
+void createTimerLoop(int period,
+        const std::vector<float>& data,
+        std::vector<std::function<float(const std::vector<float>&)>>&
+        operations) {
+    // 等待500ms
+    usleep(period * 1000);
     // 从operations中随机取出一种操作
     auto operation = operations[rand() % operations.size()];
     // 执行操作
@@ -68,19 +66,17 @@ float createTimerLoop(int period, std::vector<float>& data, std::vector<std::fun
 }
 
 // 主函数
-int main()
-{
+int main() {
     int num = 100;
     // 使用函数获取随机数据，从而可以指定数据个数
     std::vector<float> data = createRandomData(num);
-    std::vector<std::function<float (std::vector<float>&)>> operations;
+    std::vector<std::function<float(const std::vector<float>&)>> operations;
     // 可以继续写operation5、operation6等等，使用push_back添加到operations中
     operations.push_back(operation1);
     operations.push_back(operation2);
     operations.push_back(operation3);
     operations.push_back(operation4);
-    while (true)
-    {
+    while (true) {
         createTimerLoop(500, data, operations);
     }
 }
--- Folder_7_YOLO/YOLOv8/convert_dataset.py
+++ Folder_7_YOLO/YOLOv8/convert_dataset.py
@@ -13,7 +13,10 @@ def split_dataset(root_dir, train_ratio, val_ratio, test_ratio):
     os.makedirs(os.path.join(root_dir, 'test', 'labels'), exist_ok=True)
 
     # 获取所有图像和标签的文件名
-    filenames = [filename.split('.')[0] for filename in os.listdir(root_dir) if filename.endswith('.jpg')]
+    filenames = [
+        filename.split('.')[0] for filename in os.listdir(root_dir)
+        if filename.endswith('.jpg')
+    ]
 
     # 打乱文件名列表
     random.shuffle(filenames)
@@ -22,7 +25,6 @@ def split_dataset(root_dir, train_ratio, val_ratio, test_ratio):
     total_samples = len(filenames)
     train_samples = int(total_samples * train_ratio)
     val_samples = int(total_samples * val_ratio)
-    test_samples = total_samples - train_samples - val_samples
 
     # 将文件按比例分配到训练集、验证集和测试集
     train_filenames = filenames[:train_samples]
@@ -31,14 +33,21 @@ def split_dataset(root_dir, train_ratio, val_ratio, test_ratio):
 
     # 将图像和标签文件分别复制到相应的目录中
     for filename in train_filenames:
-        shutil.copy(os.path.join(root_dir, f'{filename}.jpg'), os.path.join(root_dir, 'train', 'images'))
-        shutil.copy(os.path.join(root_dir, f'{filename}.txt'), os.path.join(root_dir, 'train', 'labels'))
+        shutil.copy(os.path.join(root_dir, f'{filename}.jpg'),
+                    os.path.join(root_dir, 'train', 'images'))
+        shutil.copy(os.path.join(root_dir, f'{filename}.txt'),
+                    os.path.join(root_dir, 'train', 'labels'))
     for filename in val_filenames:
-        shutil.copy(os.path.join(root_dir, f'{filename}.jpg'), os.path.join(root_dir, 'val', 'images'))
-        shutil.copy(os.path.join(root_dir, f'{filename}.txt'), os.path.join(root_dir, 'val', 'labels'))
+        shutil.copy(os.path.join(root_dir, f'{filename}.jpg'),
+                    os.path.join(root_dir, 'val', 'images'))
+        shutil.copy(os.path.join(root_dir, f'{filename}.txt'),
+                    os.path.join(root_dir, 'val', 'labels'))
     for filename in test_filenames:
-        shutil.copy(os.path.join(root_dir, f'{filename}.jpg'), os.path.join(root_dir, 'test', 'images'))
-        shutil.copy(os.path.join(root_dir, f'{filename}.txt'), os.path.join(root_dir, 'test', 'labels'))
+        shutil.copy(os.path.join(root_dir, f'{filename}.jpg'),
+                    os.path.join(root_dir, 'test', 'images'))
+        shutil.copy(os.path.join(root_dir, f'{filename}.txt'),
+                    os.path.join(root_dir, 'test', 'labels'))
+
 
 def main(data_root):
     # 定义数据集根目录
@@ -51,9 +60,10 @@ def main(data_root):
 
     # 分割数据集
     split_dataset(dataset_root, train_ratio, val_ratio, test_ratio)
-    print("Finished Splitting Dataset!")  
+    print("Finished Splitting Dataset!")
     # 并没有删除原本在data_root中的文件，以避免搞砸了难以挽救。可以在数据集目录下使用rm *.jpg *.txt来删除
 
+
 if __name__ == '__main__':
-    data_root = input("Please input the dataset root: ")  #可输入，以后碰到这种数据集都可以用
+    data_root = input("Please input the dataset root: ")  # 可输入，以后碰到这种数据集都可以用
     main(data_root)
